a776355f58d8eb985c047e248a59ad4549c3cb0e
Assif <assif42@gmail.com>
2020-06-28 20:34:33 +0100

    add setup script

---
+++ setup_frontroom.sh
@@ -0,0 +1,12 @@
+export CC65_LIB=~/cc65/lib
+export SYMON_ROOT=~/symon
+
+export FASTCPU=1
+export VKEYB=1
+#export UCHESS2=1
+export PONG=1
+export PACMAN=1
+export BREAKOUT=1
+
+export PATH=~/cc65/bin:$PATH
+
