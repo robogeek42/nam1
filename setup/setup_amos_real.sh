435806d1d37ec1a1afeaa2046445e4a52211d2d5
assif <assif42@gmail.com>
2025-02-24 12:19:59 +0000

    Setup files for amos with real homebrew colocated

---
+++ setup/setup_amos_real.sh
@@ -0,0 +1,11 @@
+export SYMON_ROOT=~/homebrew/symon
+
+export FASTCPU=1
+export PS2K=1
+export SDIO=1
+export SOUND=1
+export UCHESS2=1
+
+export PONG=1
+export PACMAN=1
+#export IMAGETEST=1
---
+++ setup/setup_amos_symon.sh
@@ -0,0 +1,9 @@
+#export CC65_LIB=~/homebrew/cc65/lib
+export SYMON_ROOT=~/homebrew/symon
+
+export FASTCPU=1
+export VKEYB=1
+export UCHESS2=1
+export PONG=1
+export PACMAN=1
+#export IMAGETEST=1
