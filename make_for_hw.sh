144e8a050ceea4e5dcd3f9d64814faed9f4e9112
assif <assif42@gmail.com>
2026-02-10 09:56:15 +0000

    new make scripts

---
+++ make_for_hw.sh
@@ -0,0 +1,10 @@
+make clean
+#export VDP=1
+#export PS2K=1
+#export SDIO=1
+#export SOUND=1
+#export PONG=1
+#export PACMAN=1
+#export FASTCPU=1
+make all
+cp firmware homebrew.bin
---
+++ make_for_symon.sh
@@ -0,0 +1,4 @@
+make clean
+VKEYB=1 PONG=1 PACMAN=1 FASTCPU=1 make all
+cp firmware ../symon/homebrew.bin
+echo "copy to ../symon/homebrew.bin"
