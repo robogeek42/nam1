43e9d7e1d29be9151ef93ee03fb11dc50ac24c9b
assif <assif42@gmail.com>
2026-03-29 18:23:36 +0100

    Add sc2 conf code and new images. Update sort prog

--- sc2_conv/docomp.sh
+++ sc2_conv/docomp.sh
@@ -1,8 +1,4 @@
-./convcomp.exe 80SCLASS.SC2 80sClassic_COMP.inc65
-./convcomp.exe ALIEN_TO.SC2 alien_COMP.inc65
-./convcomp.exe FLOWERS1.SC2 flowers1_COMP.inc65
-./convcomp.exe FLOWERS2.SC2 flowers2_COMP.inc65
-./convcomp.exe SMASHMAR.SC2 smashmario_COMP.inc65
-./convcomp.exe STOURHEA.SC2 stourbridge_COMP.inc65
-./convcomp.exe SUZANNE.SC2 suzanne_COMP.inc65
-./convcomp.exe VP03.SC2 VP03_COMP.inc65 
+for f in CHIRONSS LAMBOMOD SLMERC SW SWPOSTER
+do
+    ./convcomp sc2files/${f}.SC2 output/${f}.BIN
+done
