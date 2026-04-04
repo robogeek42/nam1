dfe8d2ab369d47df6447d5d12bb294c72a78eea7
assif <assif42@gmail.com>
2026-04-04 13:41:51 +0100

    reduce VDP delay

--- video.s
+++ video.s
@@ -71,13 +71,9 @@ vdp_sm_loop:	LDA (ZP_TMP0), Y		; Load register value from mode tab
 				STA VDP_REGS,Y			; Save it in zero page
 .ifdef FASTCPU
 				NOP
-				NOP
-				NOP
 .endif
 				STX VDP_WR_REG
 .ifdef FASTCPU
-				NOP
-				NOP
 				NOP
 .endif
 				INY
@@ -749,7 +745,6 @@ vlsnt_loop3:	LDA #4
 vlsnt_loop2:	LDY #$20			;; count 32 chars per row
 				LDX TMP0			;; starting name value for this row
 vlsnt_loop1:	STX VDP_WR_VRAM
-				NOP
 				NOP
 				NOP
 				INX
